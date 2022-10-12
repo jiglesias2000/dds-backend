935c7699c11c0b06c95dba4cc84dafb4cfae16fb
jiglesias2000 <jiglesias.2000@gmail.com>
2022-10-12 16:36:09 -0300

    readme actualizado

--- routes/articulosfamilias.js
+++ routes/articulosfamilias.js
@@ -10,4 +10,16 @@ router.get("/api/articulosfamilias", async function (req, res, next) {
   res.json(data);
 });
 
+router.get("/api/articulosfamilias/:id", async function (req, res, next) {
+    // #swagger.tags = ['ArticulosFamilias']
+    // #swagger.summary = 'obtiene un ArticuloFamilia'
+    // #swagger.parameters['id'] = { description: 'identificador del ArticulosFamilias...' }
+    let data = await db.articulosfamilias.findAll({
+      attributes: ["IdArticuloFamilia", "Nombre"],
+      where: { IdArticuloFamilia: req.params.id },
+    });
+    if (data.length > 0 ) res.json(data[0]);
+    else res.status(404).json({mensaje:'No econtrado!!'})
+  });
+  
 module.exports = router;
