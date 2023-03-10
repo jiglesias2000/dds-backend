7942d17edd572ff1985cd528ba8867186b778226
jiglesias2000 <jiglesias.2000@gmail.com>
2023-03-10 15:14:35 -0300

    sancando var global

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const express = require("express");
 // crear servidor
 const app = express();
 
+app.use(express.json()); // para poder leer json en el body
+
+require("./base-orm/sqlite-init");  // crear base si no existe
+
 // controlar ruta
 app.get("/", (req, res) => {
   res.send("Backend inicial DDS-EXPRESS!");
 });
 
 const articulosfamiliasRouter = require("./routes/articulosfamilias");
+//const articulosfamiliasRouter = require("./routes/articulosfamiliasmock");
 app.use(articulosfamiliasRouter);
 
 // levantar servidor
