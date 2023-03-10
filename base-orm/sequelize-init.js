936e2c765be36d5888cae884a7cefb1d0fe33e14
jiglesias2000 <jiglesias.2000@gmail.com>
2023-03-10 15:19:25 -0300

    sancando var global

--- base-orm/sequelize-init.js
+++ base-orm/sequelize-init.js
@@ -1,6 +1,7 @@
 // configurar ORM sequelize
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("sqlite:" + process.env.base );
+//const sequelize = new Sequelize("sqlite:" + process.env.base );
+const sequelize = new Sequelize("sqlite:" + "./.data/pymes.db" );
 
 // definicion del modelo de datos
 const articulosfamilias = sequelize.define(
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 app.use(express.json()); // para poder leer json en el body
 
+require("./base-orm/sqlite-init"); // crear base si no existe
+
 // controlar ruta
 app.get("/", (req, res) => {
   res.send("Backend inicial dds-backend!");
